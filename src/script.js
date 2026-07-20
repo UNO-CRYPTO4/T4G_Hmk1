dbde2031fbefbf947fe02b16628e60ecc18178ab
UNO_CRYTO4 <judithagudey1@gmail.com>
2026-07-20 13:03:21 +0100

    Added changes

---
+++ src/script.js
@@ -0,0 +1,7 @@
+// Prompt the user for details
+let name = prompt("Enter your name:");
+let age = prompt("Enter your age:");
+let email = prompt("Enter your email:");
+
+// Alert the collected info
+alert(`Hi ${name}, you are ${age} years old and we'll keep in touch via your email: ${email}`);
