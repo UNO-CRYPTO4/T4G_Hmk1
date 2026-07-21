966899cea5abffa5240a3ff9d5ba1c75da33a341
UNO_CRYTO4 <judithagudey1@gmail.com>
2026-07-21 13:24:33 +0100

    Added JS variables and operations

--- src/script.js
+++ src/script.js
@@ -5,3 +5,46 @@ let email = prompt("Enter your email:");
 
 // Alert the collected info
 alert(`Hi ${name}, you are ${age} years old and we'll keep in touch via your email: ${email}`);
+
+
+// A. Store your name and log it
+let myName = "Judith Korlekie Agudey";
+console.log("Name:", myName);
+
+// B. Store your age and log it
+let myAge = 25; // replace with your actual age
+console.log("Age:", myAge);
+
+// C. Add two numbers
+let sum = 10 + 5;
+console.log("Sum:", sum);
+
+// D. Subtract two numbers
+let difference = 20 - 7;
+console.log("Difference:", difference);
+
+// E. Divide two numbers
+let quotient = 50 / 5;
+console.log("Quotient:", quotient);
+
+// F. Multiply two numbers
+let product = 6 * 8;
+console.log("Product:", product);
+
+// G. Find the remainder of 15 % 4
+let remainder = 15 % 4;
+console.log("Remainder:", remainder);
+
+// H. Store a boolean value
+let myGod = true;
+console.log("Boolean:", myGod);
+
+// I. Variable set to a number and log type
+let numberValue = 100;
+console.log("Number Value:", numberValue);
+console.log("Type of numberValue:", typeof numberValue);
+
+// J. Variable set to a string and log type
+let stringValue = "Uno Tours";
+console.log("String Value:", stringValue);
+console.log("Type of stringValue:", typeof stringValue);
